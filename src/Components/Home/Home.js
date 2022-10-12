0d569fdf966c497f677be4dc69e2e867dd81ff21
sajthewizard <rsaj53@gmail.com>
2022-10-12 12:39:25 +0200

    home header

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import './Home.css'
 
 const Home = () => {
     return (
-        <div> this is Home
+        <div className='header'>
+
+
+
+
+            <div><h3>“There are two kinds of people in this world: those who want to get things done and those who don’t want to make mistakes.”</h3></div>
+
 
         </div>
     );
