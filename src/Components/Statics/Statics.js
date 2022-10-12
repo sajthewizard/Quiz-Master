46fdcfadba6e13e30df6bc5d2090ca36273fa336
sajthewizard <rsaj53@gmail.com>
2022-10-12 18:24:14 +0200

    chart added

--- src/Components/Statics/Statics.js
+++ src/Components/Statics/Statics.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
 
 const Statics = () => {
+    const [questions, setQuestions] = useState([]); useEffect(() => {
+        axios.get('https://openapi.programming-hero.com/api/quiz')
+            .then(data => {
+                const questionsFind = data.data.data;
+                const num = questionsFind.map(que => {
+                    const id = que.id;
+                    const total = que.total; const queData = {
+                        id: id,
+                        total: total
+                    }
+                    return queData;
+                })
+                setQuestions(num);
+            })
+    }, [])
     return (
-        <div>this is statics
+        <BarChart width={550} height={400} data={questions}>
+            <Bar dataKey="total" fill="#8884d8" />
+            <XAxis datakey="Total"></XAxis>
+            <YAxis></YAxis>
+            <Tooltip></Tooltip>
+        </BarChart>
 
-        </div>
     );
 };
 
